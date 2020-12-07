ae6b9a450da7dbb083c8e7af54f977de05824491
kumgleb <iamglebk@gmail.com>
2020-12-07 19:51:48 +0000

    augmentations updated

--- data_utils.py
+++ data_utils.py
@@ -39,8 +39,9 @@ class BandsYieldDataset(Dataset):
         self.transforms = transforms
         self.augmentations = augmentations
         self.p_aug = cfg['augmentations']['p_aug']
-        self.p_flips = cfg['augmentations']['p_flips']
-        self.p_affine = cfg['augmentations']['p_affine']
+        self.p_flp = cfg['augmentations']['p_flip']
+        self.p_rot = cfg['augmentations']['p_rotate']
+        self.p_crp = cfg['augmentations']['p_crop']
 
         self.m_groups_s2 = self.create_s2_groups('S2', cfg)
         self.s2_out_dim = len(s2_bands) * len(self.m_groups_s2)
@@ -93,16 +94,12 @@ class BandsYieldDataset(Dataset):
         return bands_to_fill
 
     def apply_augmentations(self, sample):
-        rand_v =  np.random.rand(3)
-        if rand_v[0] < self.p_aug:
-          return sample
-        if self.p_flips < rand_v[1] < self.p_flips + self.p_aug:
-          sample = self.augmentations['flips'](sample)
-          return sample
-        if self.p_flips + self.p_aug < rand_v[2] < 1:
-          sample = self.augmentations['affine'](sample)
+        aug = np.random.choice(['none', 'flip', 'rotate', 'crop'],
+                              p=[self.p_aug, self.p_flp, self.p_rot, self.p_crp])
+        if aug == 'none':
           return sample
         else:
+          sample = self.augmentations[aug](sample)
           return sample
 
     def __len__(self):
@@ -176,18 +173,39 @@ class Flip(object):
         return sample
 
 
-class Affine(object):
+class Rotate(object):
     def __init__(self, cfg):
-      output_size = cfg['transforms']['s2_band_size'][0] + cfg['augmentations']['aff_pad']
-      crop_size = cfg['transforms']['s2_band_size']
+      self.aff_pad = cfg['augmentations']['aff_pad']
+      output_size = cfg['transforms']['s2_band_size'][0] + self.aff_pad
       self.scale = transforms.Resize((output_size, output_size))
-      self.rand_rot = transforms.RandomAffine(degrees=cfg['augmentations']['degrees'])
-      self.rand_crop = transforms.RandomCrop(crop_size, padding=None)
+      self.rand_rot = transforms.RandomRotation(degrees=cfg['augmentations']['degrees'])
+    
+    def crop(self, sample):
+      crop_size = self.aff_pad // 2
+      return sample[:, crop_size:-crop_size, crop_size:-crop_size]
 
     def __call__(self, sample):
-      sample = self.scale(sample)
-      sample = self.rand_rot(sample)
-      sample = self.rand_crop(sample)
+      s2_bands = sample['s2_bands']
+      s2_bands = self.scale(s2_bands)
+      s2_bands = self.rand_rot(s2_bands)
+      s2_bands = self.crop(s2_bands)
+      sample['s2_bands'] = s2_bands
+      return sample
+
+
+class Crop(object):
+    def __init__(self, cfg):
+      pad = np.random.randint(5, 32)
+      output_size = cfg['transforms']['s2_band_size'][0] + pad
+      crop_size = cfg['transforms']['s2_band_size']
+      self.scale = transforms.Resize((output_size, output_size))
+      self.rand_crop = transforms.RandomCrop(crop_size, padding=False)
+
+    def __call__(self, sample):
+      s2_bands = sample['s2_bands']
+      s2_bands = self.scale(s2_bands)
+      s2_bands = self.rand_crop(s2_bands)
+      sample['s2_bands'] = s2_bands
       return sample
 
 
@@ -217,8 +235,10 @@ def compose_transforms(s2_bands, cfg):
 
 
 def create_augmentations(cfg):
-    flip = Flip()
-    affine = Affine(cfg)
-    augmentations = {'flips': flip,
-                     'affine': affine}
+    assert (cfg['augmentations']['p_aug'] + cfg['augmentations']['p_flip'] + 
+            + cfg['augmentations']['p_rotate'] + cfg['augmentations']['p_crop']) == 1 , 'Probabilities should sum up to 1.'
+
+    augmentations = {'flip': Flip(),
+                     'rotate': Rotate(cfg),
+                     'crop': Crop(cfg)}
     return augmentations
