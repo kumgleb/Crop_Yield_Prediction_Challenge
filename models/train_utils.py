fb0024924b736ffeb468f0c47d41a9e2eb7cecba
kumgleb <iamglebk@gmail.com>
2020-12-22 08:19:56 +0000

    train utils updated, mean loss added to training monitor

--- models/train_utils.py
+++ models/train_utils.py
@@ -7,9 +7,18 @@ import torch
 
 
 def forward(model, data, criterion, device):
-    s2_bands = data['s2_bands'].to(device)
+    model_name = model.__class__.__name__
     yields = data['yield'].to(device)
-    prediction = model(s2_bands).reshape(yields.shape)
+    if model_name in ['S2BandModel', 'BandsCNN', 'HybridBandsModel', 'AutoS2BandNet', 'AutoS2BandNetShallow']:
+        s2_bands = data['s2_bands'].to(device)
+        prediction = model(s2_bands).reshape(yields.shape)
+    elif model_name in ['ClimNet']:
+        clim_bands = data['clim_bands'].to(device)
+        prediction = model(clim_bands).reshape(yields.shape)
+    elif model_name in ['S2CilmNet']:
+        s2_bands = data['s2_bands'].to(device)
+        clim_bands = data['clim_bands'].to(device)
+        prediction = model(s2_bands, clim_bands).reshape(yields.shape)
     loss = criterion(yields, prediction)
     return loss, prediction
 
@@ -52,8 +61,9 @@ def train_epoch(model, dataloader, device, optimizer, criterion, cfg):
 
 
 def train_model(model, train_dataloader, val_dataloader, device, optimizer, criterion, cfg):
-    losses_train = []
-    losses_val = []
+    losses_train, losses_train_mean = [], []
+    losses_val, losses_val_mean = [], []
+    best_val_loss = 1e6
     n_epochs = cfg['s2_train_params']['n_epochs']
 
     progress_bar = tqdm(range(n_epochs))
@@ -63,26 +73,42 @@ def train_model(model, train_dataloader, val_dataloader, device, optimizer, crit
 
         train_loss = np.mean(epoch_loss_train)
         losses_train.append(train_loss)
+        losses_train_mean.append(np.mean(losses_train))
         losses_val.append(epoch_loss_val)
+        losses_val_mean.append(np.mean(losses_val))
         progress_bar.set_description(f'train loss: {train_loss:.4f}, val loss: {epoch_loss_val:.4f}')
         clear_output(True)
         if cfg['s2_train_params']['plot_mode']:
-            train_monitor(losses_train, losses_val)
+            train_monitor(losses_train, losses_train_mean, losses_val, losses_val_mean)
+        
+        if cfg['s2_train_params']['save_best_val'] and epoch_loss_val < best_val_loss:
+          best_val_loss = epoch_loss_val
+          checkpoint_path = cfg['s2_train_params']['checkpoint_path']
+          torch.save(model.state_dict(),
+            f'{checkpoint_path}/{model.__class__.__name__}_{epoch_loss_val:.3f}')
 
 
-def train_monitor(losses_train, losses_val):
+def train_monitor(osses_train, losses_train_mean, losses_val, losses_val_mean):
     fig, ax = plt.subplots(1, 2, figsize=(18, 6))
 
     iters = np.arange(len(losses_train))
-    ax[0].plot(iters, losses_train, linewidth=2, alpha=0.8,
+    ax[0].plot(iters, losses_train, linewidth=1.5, alpha=0.6,
                c='tab:blue', label='train loss')
-    ax[0].plot(iters, losses_val, linewidth=2, alpha=0.8,
+    ax[0].plot(iters, losses_val, linewidth=1.5, alpha=0.6,
                c='tab:red', label='validation loss')
+    ax[0].plot(iters, losses_train_mean, linewidth=2, alpha=1,
+               c='tab:blue', label='mean train loss')
+    ax[0].plot(iters, losses_val_mean, linewidth=2, alpha=1,
+               c='tab:red', label='mean validation loss')
 
-    ax[1].plot(iters, losses_train, linewidth=2, alpha=0.8,
+    ax[1].plot(iters, losses_train, linewidth=1.5, alpha=0.6,
                c='tab:blue', label='train loss')
-    ax[1].plot(iters, losses_val, linewidth=2, alpha=0.8,
+    ax[1].plot(iters, losses_val, linewidth=1.5, alpha=0.6,
                c='tab:red', label='validation loss')
+    ax[1].plot(iters, losses_train_mean, linewidth=2, alpha=1,
+               c='tab:blue', label='mean train loss')
+    ax[1].plot(iters, losses_val_mean, linewidth=2, alpha=1,
+               c='tab:red', label='mean validation loss')
     ax[1].set_yscale('log')
 
     for i in [0, 1]:
