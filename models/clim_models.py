b3909eb17eb6f2a346d1caaa5967dd6995d08b7f
kumgleb <iamglebk@gmail.com>
2020-12-29 10:42:24 +0000

    minor changes

--- models/clim_models.py
+++ models/clim_models.py
@@ -1,3 +1,4 @@
+import torch
 from typing import Dict
 from torch import nn, optim, Tensor
 
@@ -28,3 +29,35 @@ class ClimNet(nn.Module):
         x = x[:, self.month_to_proc, :]
         x = self.head(x)
         return x
+
+
+class ClimCNN(nn.Module):
+  def __init__(self, cfg):
+    super(ClimCNN, self).__init__()
+    self.layer1 = nn.Sequential(
+        nn.Conv1d(13, 26, 2, 1, 1),
+        nn.ReLU(),
+        nn.Conv1d(26, 52, 2, 1, 0),
+        nn.ReLU(),
+        nn.AvgPool1d(2),
+        nn.Conv1d(52, 128, 2, 1, 1),
+        nn.ReLU(),
+        nn.AvgPool1d(2),
+        nn.Conv1d(128, 256, 2, 1, 0),
+        nn.ReLU(),
+        nn.AvgPool1d(2)
+    )
+    self.head = nn.Sequential(
+        nn.Linear(256, cfg['ClimCNN']['n_head']),
+        nn.ReLU(),
+        nn.Dropout(cfg['ClimCNN']['p_dropout']),
+        nn.Linear(cfg['ClimCNN']['n_head'], 1)
+    )
+
+
+  def forward(self, x):
+    x = x.permute(0, 2, 1)
+    x = self.layer1(x)
+    x = torch.flatten(x, 1)
+    x = self.head(x)
+    return x
